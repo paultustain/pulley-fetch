3353ea3a59d7d5e83d999c5cc59843dc94b07a69
Paul <p.tustain@hotmail.co.uk>
2025-07-20 14:15:57 +0100

    created gear mechanics with ratio.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use tetra::{Context, ContextBuilder, State};
 
 const WINDOW_WIDTH: f32 = 640.0;
 const WINDOW_HEIGHT: f32 = 480.0;
-const FRICTION: f32 = 0.005;
+const FRICTION: f32 = 0.0051;
 
 fn main() -> tetra::Result {
     ContextBuilder::new("Pong", WINDOW_WIDTH as i32, WINDOW_HEIGHT as i32)
@@ -18,39 +18,42 @@ fn main() -> tetra::Result {
 }
 
 struct GameState {
-    position: Vec2<f32>,
     score: Text,
-    gear: Gear,
+    main_gear: Gear,
+    secondary_gear: Gear,
 }
 
 impl GameState {
     fn new(ctx: &mut Context) -> tetra::Result<GameState> {
         let texture = Texture::new(ctx, "./resources/player1.png")?;
-        let position = Vec2::new(
-            (WINDOW_WIDTH - texture.width() as f32) / 2.,
+        let main_position = Vec2::new(
+            (WINDOW_WIDTH - texture.width() as f32) / 2. - 200.,
             (WINDOW_HEIGHT - texture.height() as f32) / 2.,
         );
 
-        let gear = Gear::new(texture, position, 0.);
+        let main_gear = Gear::new(texture.clone(), main_position, 0., 28.);
+        let secondary_gear = Gear::new(
+            texture.clone(),
+            Vec2::new(main_position.x + 400., main_position.y),
+            0.,
+            9.,
+        );
 
-        let clicks = 0;
         Ok(GameState {
-            position: position,
             score: Text::new(
                 format!("Rotation: {}", 0.),
                 Font::vector(ctx, "./resources/DejaVuSansMono.ttf", 16.)?,
             ),
-            gear: gear,
+            main_gear: main_gear,
+            secondary_gear: secondary_gear,
         })
     }
 }
 
 impl State for GameState {
     fn update(&mut self, ctx: &mut Context) -> tetra::Result {
-        self.position = input::get_mouse_position(ctx).round();
-
         if is_mouse_button_pressed(ctx, MouseButton::Left) {
-            self.gear.rotation_speed = 0.1;
+            self.main_gear.rotation_speed = 0.1;
         }
         Ok(())
     }
@@ -58,27 +61,39 @@ impl State for GameState {
     fn draw(&mut self, ctx: &mut Context) -> tetra::Result {
         graphics::clear(ctx, Color::rgb(0.769, 0.812, 0.631));
 
-        self.gear.rotation += PI as f32 * self.gear.rotation_speed;
-        self.gear.rotation_speed -= self.gear.friction;
-        if self.gear.rotation_speed < 0. {
-            self.gear.rotation_speed = 0.;
+        let ratio = self.main_gear.teeth / self.secondary_gear.teeth;
+
+        self.main_gear.rotation += PI as f32 * self.main_gear.rotation_speed;
+        self.secondary_gear.rotation += PI as f32 * (self.main_gear.rotation_speed * ratio);
+
+        self.main_gear.rotation_speed -= self.main_gear.friction;
+        if self.main_gear.rotation_speed < 0. {
+            self.main_gear.rotation_speed = 0.;
         }
 
         let new_score = format!(
             "Score: {}",
-            self.gear.rotation as f64 / (2. * std::f64::consts::PI)
+            self.secondary_gear.rotation as f64 / (2. * std::f64::consts::PI)
         );
 
         self.score.set_content(&new_score);
         self.score.draw(ctx, Vec2::new(16., 16.));
-        self.gear.texture.draw(
+        self.main_gear.texture.draw(
             ctx,
             DrawParams::new()
-                .position(self.gear.position)
-                .origin(self.gear.center())
-                .rotation(self.gear.rotation),
+                .position(self.main_gear.position)
+                .origin(self.main_gear.center())
+                .rotation(self.main_gear.rotation),
         );
 
+        self.secondary_gear.texture.draw(
+            ctx,
+            DrawParams::new()
+                .position(self.secondary_gear.position)
+                .origin(self.secondary_gear.center())
+                .scale(Vec2::new(1. / ratio, 1. / ratio))
+                .rotation(self.secondary_gear.rotation),
+        );
         Ok(())
     }
 }
@@ -89,16 +104,18 @@ struct Gear {
     rotation: f32,
     friction: f32,
     rotation_speed: f32,
+    teeth: f32,
 }
 
 impl Gear {
-    fn new(texture: Texture, position: Vec2<f32>, rotation: f32) -> Gear {
+    fn new(texture: Texture, position: Vec2<f32>, rotation: f32, teeth: f32) -> Gear {
         Gear {
             texture,
             position,
             rotation,
             friction: FRICTION,
             rotation_speed: 0.,
+            teeth: teeth,
         }
     }
 
